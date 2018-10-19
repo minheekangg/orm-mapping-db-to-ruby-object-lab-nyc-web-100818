eb615f73b384a04f22eacb2bfe34fcfdca12e280
Min Hee  Park <minheekangg@gmail.com>
2018-10-19 14:05:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
 
   def self.new_from_db(row)
     new_song = Student.new
-    new_song.id = row[0]
+    new_song.id = row[0].ro_i
     new_song.name = row[1]
     new_song.grade = row[2]
 
